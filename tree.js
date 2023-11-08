54ea8f7634188196201f5463ce1bb8774cc6ad31
Matthew Baer <matt.baer77@gmail.com>
2023-11-08 00:01:26 -0500

    completed tree

--- tree.js
+++ tree.js
@@ -15,12 +15,62 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
+
+    let sum = 0;
+
+    let toSumQueue = [this.root];
+
+    while (toSumQueue.length) {
+
+      let current = toSumQueue.shift();
+
+      if (current) {
+
+        sum = sum + current.val
+
+        for (let child of current.children){
+          toSumQueue.push(child)
+        }
+
+      }
+
+    }
+
+    return sum;
     
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+
+    let numOfEvens = 0;
+
+    let toCheckQueue = [this.root];
+
+    while (toCheckQueue.length) {
+
+      let current = toCheckQueue.shift();
+
+      if (current) {
+
+        if(!(current.val % 2)) {
+
+          numOfEvens++
+
+        }
+
+        for (let child of current.children){
+
+          toCheckQueue.push(child)
+
+        }
+
+      }
+
+    }
+
+    return numOfEvens;
 
   }
 
@@ -29,7 +79,36 @@ class Tree {
 
   numGreater(lowerBound) {
 
+    let numOfGreater = 0;
+
+    let toCheckQueue = [this.root];
+
+    while (toCheckQueue.length) {
+
+      let current = toCheckQueue.shift();
+
+      if (current) {
+
+        if(current.val > lowerBound) {
+
+          numOfGreater++
+
+        }
+
+        for (let child of current.children){
+
+          toCheckQueue.push(child)
+
+        }
+
+      }
+
+    }
+
+    return numOfGreater;
+
   }
+
 }
 
 module.exports = { Tree, TreeNode };
